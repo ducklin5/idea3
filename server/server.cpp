d8a3a774c4a7cf3ae25afe8f835de1ad189ea1a8
cmput274 <abassazeez6@gmail.com>
2019-03-24 19:35:23 -0600

    Good shit

--- server/server.cpp
+++ server/server.cpp
@@ -105,15 +105,15 @@ Mat blobExtract( Mat binImg, map<int,int>& stats){
 			//cout << "label: " << label << endl;
 		}
 	}
-
-//	for (auto const& eqv : labelEqvs) {
-//		cout << eqv.first << ':';
-//		for (auto const& lbl : eqv.second){
-//			cout << lbl << ", ";
-//		}
-//		cout << std::endl ;
-//	}
-//
+/*
+	for (auto const& eqv : labelEqvs) {
+		cout << eqv.first << ':';
+		for (auto const& lbl : eqv.second){
+			cout << lbl << ", ";
+		}
+		cout << std::endl ;
+	}
+*/
 	map <int,int> simpleEqvs;
 	
 	for (auto const& eqv: labelEqvs){
@@ -127,13 +127,13 @@ Mat blobExtract( Mat binImg, map<int,int>& stats){
 		}
 		simpleEqvs[eqv.first] = crnt;
 	}
-//	
-//	for (auto const& eqv : simpleEqvs) {
-//		cout << eqv.first << " : ";
-//		cout << eqv.second << '\n';
-//	}
-//
-
+/*
+	for (auto const& eqv : simpleEqvs) {
+		cout << eqv.first << " : ";
+		cout << eqv.second << '\n';
+	}
+*/
+	// second pass
 	for (int y = 0; y < blobImg.rows; y++){
 		for (int x = 0; x < blobImg.cols; x++){
 			int& currentLabel = blobImg.at<int>(y,x);
@@ -146,12 +146,19 @@ Mat blobExtract( Mat binImg, map<int,int>& stats){
 			}
 		}
 	}
-//	for (auto const& eqv : stats) {
-//		cout << eqv.first << " : ";
-//		cout << eqv.second << '\n';
-//	}
+/*
+	for (auto const& eqv : stats) {
+		cout << eqv.first << " : ";
+		cout << eqv.second << '\n';
+	}
+*/
 	cout << stats.size() << " labels\n";;
 	return blobImg;
+}
+
+template <typename fType, typename sType>
+bool comparePair2 (const pair<fType, sType>& p1, const pair<fType, sType>& p2){
+	return p1.second < p2.second;
 }
 
 int main(int argc, char** argv )
@@ -163,34 +170,78 @@ int main(int argc, char** argv )
 	
 	Mat gray;
 	cvtColor(orig, gray, CV_RGB2GRAY);
+	imshow("Gray", gray);
 
-	// radiud based thersholding
-	Mat black = threshold(gray, 4);
+	// radius based thersholding
+	Mat black = threshold(gray, 5);
 
 	// blob extraction - https://en.wikipedia.org/wiki/Connected-component_labeling
 	// graph theory
 	map<int, int> stats;
-	Mat blobbed = blobExtract(black, stats);
+	Mat blobs = blobExtract(black, stats);
+	imshow("B&W", black);
 	
+	// find the blob label with the most elements
 	//https://stackoverflow.com/questions/30611709/find-element-with-max-value-from-stdmap
 	stats[0] = 0;
-	auto maxlbl_count = std::max_element(stats.begin(), stats.end(),
-			[](const pair<int, int>& p1, const pair<int, int>& p2) {return p1.second < p2.second; }
-			);
+	auto maxlbl_count = std::max_element(stats.begin(), stats.end(), comparePair2<int,int>);
 	int maxlbl = maxlbl_count -> first;
-	
 	cout << "maxlbl: " << maxlbl << endl;
-	for (int y = 0; y < blobbed.rows; y++){
-		for (int x = 0; x < blobbed.cols; x++){
-			int& current = blobbed.at<int>(y,x);
-			current = ( current == maxlbl? 2550000 : 0);
-		}
-	}	
+	
+	imshow("Blobs", blobs);
+	waitKey('0');
+	// isolate the largest blob
+	// and calculate the distances from the img corners to the largest blob pixels
+
+	// this is where the edit image will go
+	Mat majorBlob(blobs.size(), CV_8UC1, Scalar(0));
+	// save the corners of the image
+	vector<Point> imageConers = {
+		Point(0,0),
+		Point(blobs.cols, 0),
+		Point(blobs.cols, blobs.rows),
+		Point(0, blobs.rows)
+	};
+	// create 4 matrices to hold the distances for each pixel
+	Mat dists[4];
+		for (int i=0; i<4; i++)
+			dists[i] = Mat(blobs.size(), CV_32SC1, Scalar(0));
 	
 
-	imshow("Gray", gray);
-	imshow("B&W", black);
-	imshow("Blobbed", blobbed);
-	waitKey(0);
+	for (int y = 0; y < blobs.rows; y++){
+		for (int x = 0; x < blobs.cols; x++){
+			int& current = blobs.at<int>(y,x);
+			uchar& mcurrent = majorBlob.at<uchar>(y,x);
+			if (current == maxlbl){
+				mcurrent = 1;
+				for(int i=0; i<4; i++){
+					cout << imageConers[i].x << ", " << imageConers[i].y << "\n";
+					dists[i].at<int>(y,x) = abs(y - imageConers[i].y) + abs(x - imageConers[i].x);
+				}
+			} else {
+				mcurrent = 0;
+			}
+		}
+	}
+	imshow("majorBlob", majorBlob);
+	
+	for(int i = 0; i < 4; i++){
+		Mat test;
+		dists[i].convertTo(test, CV_8UC1, 0.2);
+		imshow(to_string(i), test);
+	}
 
+	// find the minimum for each corner
+	Mat mask = majorBlob.clone();
+	vector<Point> sdkCorners (4);
+	for(int i = 0; i < 4; i++){
+		minMaxLoc(dists[i], NULL, NULL, &sdkCorners[i], NULL, mask);
+		circle(majorBlob, sdkCorners[i], 10, 1);
+	}
+	imshow("majorBlobCorners", majorBlob*255);
+	
+
+
+	waitKey(0);
+	exit(0);
 }
