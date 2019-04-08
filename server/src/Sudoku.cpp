0ff7950cb747515218e7e2574b00a31d33dd8885
cmput274 <abassazeez6@gmail.com>
2019-04-07 23:06:55 -0600

    pre submission

--- server/src/Sudoku.cpp
+++ server/src/Sudoku.cpp
@@ -2,42 +2,55 @@
 #include <set>
 #include "digraph.h"
 
+// This file contains the sudoku puzzle class itself which utilizes the cell struct, the relations struct and the cellgraph class
+// in addition to the methods that will be used to create, populate and solve the puzzle
+
 bool verbose = 0;
 
+// Key value map used later when solving the puzzle
 typedef map<int,vector<Cell*>> intVecCellMap;
+
 class Sudoku {
 	private:
 		CellGraph graph;
+		// size and physical properties
 		int rows;
 		int cols;
 		int gwidth;
 		int gheight;
+		// map containing the number of groups and the different cells in each group
 		map<int,vector<Cell*>> groups;
 
 	public:
+		// puzzle constructor with the default puzzle being a 3x3 puzzle 
 		Sudoku(int r = 3, int c = 3, int gw = 3, int gh = 3 ){
 			rows = r; gwidth = gw;
 			cols = c; gheight = gh;
+			
+			// create all the cell in every row and column
 			for(int y = 0; y < rows; y++){
 				for(int x = 0; x < cols; x++){
-					int n = calcGroup(x,y);
+					int n = calcGroup(x,y); 
 					Cell* A = graph.addCell(x,y,n);
 
-					groups[n].push_back(A);
+					groups[n].push_back(A); // add each cell to their respective group
 				}
 			}
 		}
-
+		
+		// Method to calculate each cells group number based on the group height and width and the number of rows and columns
 		int calcGroup(int x, int y){
 			int colGroups = cols/gwidth;
 			return x/gwidth + colGroups * (y/gheight);
 		}
-
+		
+		// Method to set the value (i.e. the number stored in them) of each cell
 		void setVal(int x, int y, int k){
 			Cell* B = graph.getCell(x,y);
 			B->k = k;
 		}
-
+		
+		// Method to populat all the cells in the puzzle with their given values if they have one, and 0 if they dont have any.
 		void populate(vector<int> input){
 			for(int y = 0; y < rows; y++){
 				for(int x = 0; x < cols; x++){
@@ -45,13 +58,17 @@ class Sudoku {
 				}
 			}
 		}
-
+		
+		// Method to get a set of cell pointers to the relations of the input cell
 		set<Cell*> getRelatives(Cell* input){
 			int x = input -> x;
 			int y = input -> y;
 			set<Cell*> relatives;
+			
+			// iterates through every column in the fixed row and gets all the relatives of the input cell, i.e. gets all the cells
+			// in the same row, column and group as the input cell. Does the same for every row int the fixed column
 			for(int i = 0; i < cols; i++ ){
-				relatives.insert(graph.getCell(i,y));
+				relatives.insert(graph.getCell(i,y)); 
 			}
 			for(int j = 0; j < rows; j++){
 				relatives.insert(graph.getCell(x,j));
@@ -60,7 +77,7 @@ class Sudoku {
 			for ( auto groupCell : groups[n] ){
 				relatives.insert(groupCell);
 			}
-			relatives.erase(input);
+			relatives.erase(input); // makes sure teh input cell is not included as a relative to itself
 			return relatives;
 		}
 		void hLine(){
@@ -70,6 +87,8 @@ class Sudoku {
 			}
 			cout << "+\n";
 		}
+		
+		// draws the sudoku puzzle 
 		void draw (){
 			for(int j = 0; j < rows; j++){
 				if(j % gheight == 0) hLine();
@@ -90,10 +109,16 @@ class Sudoku {
 			hLine();
 		}
 
+		// Method to create edges (Relations) between verices.
 		void makeRelations(){
 			vector<Cell*> cells = graph.getCellPtrs();
+			
+			// Iterate through the vector of cells checking for cells that do not have a 0 value stored in them
 			for(auto A: cells){
 				if (A->k == 0) continue;
+				
+				// if they dont have a value of 0, then make a connection between it and another cell whose k value is 0 and is not already
+				// connected to another cell with the same value
 				for(auto B : getRelatives(A)){
 					if (B->k > 0) continue;
 					bool alreadyConnected = false;
@@ -102,17 +127,23 @@ class Sudoku {
 						alreadyConnected = (r.k == A->k);
 						if (alreadyConnected) break;
 					}
-
+					
+					// if the second cell is already connected to a cell with the same value, then dont make another relation
 					if(alreadyConnected) continue;
-					graph.addRelation(A,B, A->k);
+					graph.addRelation(A,B, A->k); // add a relation between both cells
 				}
 			}
 		}
-
+		
+		// Method to get possibility map used to solve the puzzle. Possibility map is a key value map of all the cells and how many solutions they have
 		intVecCellMap getPsbltyMap(){
 			int kolors = gwidth * gheight;
 			vector<Cell*> cells = graph.getCellPtrs();
 			intVecCellMap psbltyMap;
+			
+			// iterate through all the cells with a value of 0 in the puzzle, going through and counting the number of relatives they have and calculating
+			// the number of of solutions they have which is gotten by subtracting the number ofrelations they have from the total number of possible relations
+			// Add each cell as a value to their respective possibility count key, then return the map.
 			for(auto A: cells){
 				if (A->k > 0) continue;
 				int cnvrgCount = A->relations.size();
@@ -121,26 +152,33 @@ class Sudoku {
 			}
 			return psbltyMap;
 		}
-
+				
+		// Method to solve/calculate the value of all the cells with only one possible solution.
 		void solveOnePsblty(intVecCellMap psbltyMap){
-			// get the cell
+			// iterate through all the cells with only one possible solution and create a set of the weight of all its relations, with the weight being
+			// the value of the cell it is related to
 			for (auto pCell:psbltyMap[1]){
 				if(verbose) cout << "one solution: " << *pCell << "\n";
 				set<int> connectedKs;
 				for(auto r:pCell->relations){
 					connectedKs.insert(r.k);
 				}
+				// iterate through the weight of all the relations and look for the weight that is not in the set, that will be the value of the cell.
 				int newK;
 				for(newK = 1; connectedKs.count(newK); newK++);
 
 				if(verbose) cout << "solution: " << newK << "\n";
 				pCell->k = newK;
 			}
+
 		}
 		
+		// Method to solve/ calculate the value of the rest of the puzzle i.e. the cells that have more than one possible solution
 		void solveLeastPsblty(intVecCellMap psbltyMap, int leastKey){
+			// get all the cells with the possible solutions specified in the input.
 			vector<Cell*> lPsbltyCells = psbltyMap[leastKey];
-			// get the best candidate
+			// get the best candidate, by checking which cell has the most number of weighted relatives. Whichever cells that is,
+			// is the best candidate to be solved next.
 			int maxCount = -1; Cell* lPsbltyCell;
 			for(auto lpCell : lPsbltyCells){
 				int filledCount = 0;
@@ -159,6 +197,8 @@ class Sudoku {
 			if(verbose) cout << leastKey 
 				<< " solutions: " 
 					<< *lPsbltyCell << "\n";
+			// calculate the value of the cell with the least possibilities in a very similar fashion as the single possibility cell
+			// except there are more guesses to be made, because there is more than one possible soluti  on
 			set<int> connectedKs;
 			for(auto r:lPsbltyCell->relations){
 				connectedKs.insert(r.k);
@@ -170,8 +210,10 @@ class Sudoku {
 
 		}
 
+		// method that puts together all the previous steps in order to fully solve the sudoku puzzle
 		void solve(){
 			bool solved = false;
+	
 			while(!solved){
 				// make all neccesary relations
 				makeRelations();
@@ -188,13 +230,19 @@ class Sudoku {
 					}
 					cout << "----------\n";
 				}
+				// if the possibility map (map of all the cells with no value in them and their possible solutions) is empty,
+				// then it means the puzzle has already been solved and the while loop breaks. If the possibility map is filled 
+				// with 0 or there is at least an occurence of it, it means there is a cell with no possible solutions, hence
+				// the puzzle is unsolvable and break the while loop
 				if(psbltyMap.size() == 0){
 					solved = true; break;
 				} else if ( psbltyMap.count(0) ){
+				
 					break;
 				}
 
-				// check if there are Cells with only 1 possible solution
+				// check if there are Cells with only 1 possible solution and then solve those cells, then check for the 
+				// cells with the next least possible solution and solve it, till the puzzle is solved
 				int leastKey = psbltyMap.begin()->first;
 				if(verbose) cout << "least posibilities: " << leastKey << "\n";
 				if (leastKey == 1){
@@ -203,7 +251,7 @@ class Sudoku {
 					solveLeastPsblty(psbltyMap, leastKey);
 				} 
 			}
-			if(!solved){
+			if(!solved){	// print an error message if the puzzle is unsolvable
 				cout << "PUnsolvable puzzle\n";
 			}
 		}
--- server/src/digraph.h
+++ server/src/digraph.h
@@ -4,12 +4,18 @@
 #include <iostream> // for printing cell structures
 #include <cassert> // assert, error check
 
+// This file contains the description and definition of the Cell struct used in the main sudoku puzzle and the puzzle solver
+
+
 using namespace std;
 
-// A vertex structure
+// A vertex structure that represents a cell in the sudoku puzzle
 struct Cell;
 
-// An edge structure
+// An edge structure that connects two cells. This particular relation is bidirectional i.e. if cell A has a relation with cell B,
+// then cell B has that same relation with cell A. Relations are simply any cell that is located in the same row or column or group
+// as another
+ 
 struct Relation{
 	Cell* from;
 	Cell* to;
@@ -17,7 +23,7 @@ struct Relation{
 	Relation( Cell* fromC, Cell* toC, int kvar = 0 ) {
 		from = fromC;
 		to = toC;
-		k = kvar;
+		k = kvar; // weighting of the edge. 
 	}
 };
 
@@ -28,13 +34,16 @@ struct Cell{
 	int n;
 	// value property
 	int k;
-	vector<Relation> relations;
-	Cell(int xvar, int yvar, int nvar = 0, int kvar = 0) {
+	vector<Relation> relations; // vector storing all the relations it has
+	Cell(int xvar, int yvar, int nvar = 0, int kvar = 0) {	// cell constructor
 		x = xvar;
 		y = yvar;
 		k = kvar;
 		n = nvar;
 	}
+	
+	// Method to add a relation to the vector of relations possesed by a cell
+	
 	void addRelation(Cell* to, int kVar=0){
 		Relation newRelation(this, to, kVar);
 		relations.push_back(newRelation);
@@ -49,26 +58,34 @@ ostream& operator<< (ostream& os, const Cell& sElem) {
 	return os;
 }
 
-// graph class using Cell vertex and Relation Edges
+// graph class using the Cell vertex struct and and Relation Edges struct
 class CellGraph{
 	public:
+	
+		// Method to add cell to the private pairwise integer and cell pointer map
 		Cell* addCell(int x, int y, int n = 0){
-			// check if a pointer to that cell already exists
+			// check if a pointer to that cell already exists and return the cell pointer if it already does
 			if (cells.count({x,y}) > 0)
 				return cells[{x,y}];
-			// inserts a new cell into the graph
+			// inserts a new cell into the graph with the coordinates speciffied
 			Cell* newCellPtr = new Cell(x,y,n);
+			// add the newly created cell to the pairwise integer and cell pointer map
 			cells[{x,y}] = newCellPtr;
 			return newCellPtr;
 		}
+
+		// Method to look for and return a cell from the private map
 		Cell* getCell(int x, int y){
-			bool exists = cells.count({x,y}) > 0;
+			bool exists = cells.count({x,y}) > 0; // check if there is an occurence of the cell in the map and return true if there is
 			if(!exists){
 				cout << x << ", " << y;
 			}
-			assert (exists && "No such cell in graph");
-			return cells[make_pair(x,y)];
+			assert (exists && "No such cell in graph"); // print out an error message if no such cell exists
+			return cells[make_pair(x,y)]; // return the x and y coordinates of the cell if it exists
 		}
+		
+		// Method to add a bidirectional relation to a cell by first of all adding the relation from A to B and then from B to A
+		// ensuring the relation edge is bidirectional
 		void addRelation(Cell* cellA, Cell* cellB, int k){
 			Cell* A = addCell(cellA->x, cellA->y);
 			Cell* B = addCell(cellB->x, cellB->y);
@@ -77,13 +94,15 @@ class CellGraph{
 			B->addRelation(A, k);
 		}
 
+		// Method to get a list of all the cell pointers in the map. Returns a vector of cell pointers
 		vector<Cell*> getCellPtrs() {
 			vector<Cell*> cellPtrVec;
 			 for(auto elem : cells)
 				 	 cellPtrVec.push_back(elem.second);
 			return cellPtrVec;
 		}
-
+		
+		// Printing the details of a cell
 		void print(){
 			for(auto elem: cells){
 				cout << *elem.second;
@@ -95,6 +114,8 @@ class CellGraph{
 				cout << "\n";
 			}
 		}
+	
+	// Key value Map that stores the position of a cell as its key and the cell pointer as its value
 	private:
 		map<pair<int,int>, Cell*> cells;
 };
